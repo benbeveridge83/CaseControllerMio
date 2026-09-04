eddfcd7ff1deee04a0625194650438b8443576cc
benbeveridge83 <beveridgeben@gmail.com>
2026-09-04 15:50:44 -0500

    Enable Mio V268 calculated trust account build transform

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mioV268Transform from './mio-v268-transform.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [mioV268Transform(), react()],
 })
