baebbefa8df3f50e8291c4cd58e9f41411c6334e
benbeveridge83 <beveridgeben@gmail.com>
2026-09-04 16:00:01 -0500

    Enable V268 calculated trust hotfix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mioV268Transform from './mio-v268-transform.js'
+import mioV268Hotfix from './mio-v268-hotfix.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [mioV268Transform(), react()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), react()],
 })
