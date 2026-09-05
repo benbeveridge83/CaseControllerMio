7d45acc2d9a27ea13c3db9a1bac02fdd5e4a378d
benbeveridge83 <beveridgeben@gmail.com>
2026-09-04 19:47:21 -0500

    Enable Mio V275 Word-like template editor

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import mioV268Transform from './mio-v268-transform.js'
 import mioV268Hotfix from './mio-v268-hotfix.js'
 import mioV272DraftingFormatting from './mio-v272-drafting-formatting.js'
 import mioV274DraftingLayout from './mio-v274-drafting-layout.js'
+import mioV275DraftingEditor from './mio-v275-drafting-editor.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV274DraftingLayout(), react()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV274DraftingLayout(), mioV275DraftingEditor(), react()],
 })
