253e8f3c55e7d61396b0641ee4b404c074743d2f
benbeveridge83 <beveridgeben@gmail.com>
2026-09-04 19:13:28 -0500

    Enable V271 tolerant Word layout preservation

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mioV268Transform from './mio-v268-transform.js'
 import mioV268Hotfix from './mio-v268-hotfix.js'
-import mioV270DraftingFormatting from './mio-v270-drafting-formatting.js'
+import mioV271DraftingFormatting from './mio-v271-drafting-formatting.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [mioV268Transform(), mioV268Hotfix(), mioV270DraftingFormatting(), react()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), mioV271DraftingFormatting(), react()],
 })
