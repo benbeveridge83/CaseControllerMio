069ef92a1b9e51da751dee3fcb0079171a369987
benbeveridge83 <beveridgeben@gmail.com>
2026-09-04 19:15:31 -0500

    Enable V273 Word table and list formatting preview

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import mioV268Transform from './mio-v268-transform.js'
 import mioV268Hotfix from './mio-v268-hotfix.js'
 import mioV272DraftingFormatting from './mio-v272-drafting-formatting.js'
+import mioV273DraftingTables from './mio-v273-drafting-tables.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), react()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV273DraftingTables(), react()],
 })
