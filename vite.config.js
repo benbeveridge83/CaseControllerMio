f54b884e3210a074a130a6d49d922b6e39db100f
benbeveridge83 <beveridgeben@gmail.com>
2026-09-04 19:14:14 -0500

    Enable V272 safe Drafting Studio formatting fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mioV268Transform from './mio-v268-transform.js'
 import mioV268Hotfix from './mio-v268-hotfix.js'
-import mioV271DraftingFormatting from './mio-v271-drafting-formatting.js'
+import mioV272DraftingFormatting from './mio-v272-drafting-formatting.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [mioV268Transform(), mioV268Hotfix(), mioV271DraftingFormatting(), react()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), react()],
 })
