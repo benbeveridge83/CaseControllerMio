36cde4316684a6f32960a5b829f982446e6065a6
benbeveridge83 <beveridgeben@gmail.com>
2026-09-04 20:01:18 -0500

    Enable Mio V276 categorized auto-fill picker

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import mioV268Hotfix from './mio-v268-hotfix.js'
 import mioV272DraftingFormatting from './mio-v272-drafting-formatting.js'
 import mioV274DraftingLayout from './mio-v274-drafting-layout.js'
 import mioV275DraftingEditor from './mio-v275-drafting-editor.js'
+import mioV276AutofillPicker from './mio-v276-autofill-picker.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV274DraftingLayout(), mioV275DraftingEditor(), react()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV274DraftingLayout(), mioV275DraftingEditor(), mioV276AutofillPicker(), react()],
 })
