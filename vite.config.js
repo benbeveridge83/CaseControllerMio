0c2e7f2a68801a1103293cde52b5be492f9d25e4
benbeveridge83 <beveridgeben@gmail.com>
2026-09-04 19:10:53 -0500

    Enable V270 Word formatting preservation

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mioV268Transform from './mio-v268-transform.js'
 import mioV268Hotfix from './mio-v268-hotfix.js'
+import mioV270DraftingFormatting from './mio-v270-drafting-formatting.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [mioV268Transform(), mioV268Hotfix(), react()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), mioV270DraftingFormatting(), react()],
 })
