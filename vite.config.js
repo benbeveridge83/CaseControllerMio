2fb052616f98b290718ffba9d4583a9c733f821a
benbeveridge83 <beveridgeben@gmail.com>
2026-09-05 16:31:09 -0500

    Enable Mio V292 nonblocking drafting

--- vite.config.js
+++ vite.config.js
@@ -15,8 +15,9 @@ import mioV283WithdrawalClarity from './mio-v283-withdrawal-clarity.js'
 import mioV284DraftingStructuralDelete from './mio-v284-drafting-structural-delete.js'
 import mioV287DraftingReadiness from './mio-v287-drafting-readiness.js'
 import mioV291DraftingCleanUi from './mio-v291-drafting-clean-ui.js'
+import mioV292DraftingNonblocking from './mio-v292-drafting-nonblocking.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV274DraftingLayout(), mioV275DraftingEditor(), mioV276AutofillPicker(), mioV277CloudPersistence(), mioV278DraftingComponents(), mioV279WithdrawalDashboard(), mioV280DraftingDocumentSetup(), mioV281DraftingBlocks(), mioV283WithdrawalClarity(), mioV284DraftingStructuralDelete(), mioV287DraftingReadiness(), mioV291DraftingCleanUi(), react()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV274DraftingLayout(), mioV275DraftingEditor(), mioV276AutofillPicker(), mioV277CloudPersistence(), mioV278DraftingComponents(), mioV279WithdrawalDashboard(), mioV280DraftingDocumentSetup(), mioV281DraftingBlocks(), mioV283WithdrawalClarity(), mioV284DraftingStructuralDelete(), mioV287DraftingReadiness(), mioV291DraftingCleanUi(), mioV292DraftingNonblocking(), react()],
 })
