a6bb27b72221a736f44d2086f4359001b13f2570
benbeveridge83 <beveridgeben@gmail.com>
2026-09-07 13:48:43 -0500

    Enable withdrawal flow and graph status controls

--- vite.config.js
+++ vite.config.js
@@ -26,9 +26,10 @@ import mioV303WithdrawalNextAction from './mio-v303-withdrawal-next-action.js'
 import mioV304WithdrawalInline from './mio-v304-withdrawal-inline.js'
 import mioV3042WithdrawalSimpleComplete from './mio-v3042-withdrawal-simple-complete.js'
 import mioV3043WithdrawalGlance from './mio-v3043-withdrawal-glance.js'
+import mioV3044WithdrawalFlow from './mio-v3044-withdrawal-flow.js'
 import mioBrowserSyntaxCheck from './mio-browser-syntax-check.js'
 
 // V298/V299 stay disabled. V300 is isolated on the repair branch pending all verification gates.
 export default defineConfig({
-  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV274DraftingLayout(), mioV275DraftingEditor(), mioV276AutofillPicker(), mioV277CloudPersistence(), mioV278DraftingComponents(), mioV279WithdrawalDashboard(), mioV280DraftingDocumentSetup(), mioV281DraftingBlocks(), mioV283WithdrawalClarity(), mioV284DraftingStructuralDelete(), mioV287DraftingReadiness(), mioV291DraftingCleanUi(), mioV292DraftingNonblocking(), mioV293TemplateMatterTest(), mioV294TemplateOpenDownloadFix(), mioV295BindingReanchor(), mioV300TemplateRepair(), mioV301StartupSettings(), mioV302PartyPronouns(), mioV303WithdrawalNextAction(), mioV304WithdrawalInline(), mioV3042WithdrawalSimpleComplete(), mioV3043WithdrawalGlance(), react(), mioBrowserSyntaxCheck()],
+  plugins: [mioV268Transform(), mioV268Hotfix(), mioV272DraftingFormatting(), mioV274DraftingLayout(), mioV275DraftingEditor(), mioV276AutofillPicker(), mioV277CloudPersistence(), mioV278DraftingComponents(), mioV279WithdrawalDashboard(), mioV280DraftingDocumentSetup(), mioV281DraftingBlocks(), mioV283WithdrawalClarity(), mioV284DraftingStructuralDelete(), mioV287DraftingReadiness(), mioV291DraftingCleanUi(), mioV292DraftingNonblocking(), mioV293TemplateMatterTest(), mioV294TemplateOpenDownloadFix(), mioV295BindingReanchor(), mioV300TemplateRepair(), mioV301StartupSettings(), mioV302PartyPronouns(), mioV303WithdrawalNextAction(), mioV304WithdrawalInline(), mioV3042WithdrawalSimpleComplete(), mioV3043WithdrawalGlance(), mioV3044WithdrawalFlow(), react(), mioBrowserSyntaxCheck()],
 })
